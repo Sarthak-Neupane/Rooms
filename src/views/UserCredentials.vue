430da5d863a0c2c4047f670a2799a16cf5677fa0
Sarthak Neupane <sarthakrocks2004@gmail.com>
2022-08-20 21:05:15 +0545

    credentials authentication done

--- src/views/UserCredentials.vue
+++ src/views/UserCredentials.vue
@@ -4,19 +4,29 @@
   >
     <h1 class="text-dark-text">Just A Few Things More ...</h1>
     <base-card class="bg-dark-secondary p-7">
-      <form class="flex flex-col justify-center items-start gap-7">
+      <form
+        class="flex flex-col justify-center items-start gap-7"
+        @submit.prevent="formSubmit()"
+      >
         <div class="flex flex-col justify-center-items-start gap-5 w-full">
           <label
             for="username"
             class="text-dark-text sm:text-xs lg:text-sm 2xl:text-lg"
           >
             Username
+            <span
+              class="sm:text-xs text-red-400 ml-3"
+              v-if="errorMessageRef === 1 && errorMessage"
+            >
+              {{ errorMessage }}
+            </span>
           </label>
           <input
             type="text"
             name="username"
             id="username"
             class="w-11/12 text-dark-text bg-dark-primary h-10 focus:outline-none p-2"
+            v-model.trim="username"
           />
         </div>
         <div class="flex flex-col justify-center-items-start gap-5 w-full">
@@ -25,12 +35,20 @@
             class="text-dark-text sm:text-xs lg:text-sm 2xl:text-lg"
           >
             Display Name
+            <span
+              class="sm:text-xs text-red-400 ml-3"
+              v-if="errorMessageRef === 2 && errorMessage"
+            >
+              {{ errorMessage }}
+            </span>
           </label>
           <input
             type="text"
             name="displayName"
             id="displayName"
             class="w-11/12 text-dark-text bg-dark-primary h-10 focus:outline-none p-2"
+            v-model.trim="displayName"
+            @input="checkDisplayName()"
           />
         </div>
 
@@ -39,11 +57,18 @@
             <h1 class="text-dark-text sm:text-xs lg:text-sm 2xl:text-lg">
               Display Picture
             </h1>
+            <span
+              class="sm:text-xs text-red-400 ml-3"
+              v-if="errorMessageRef === 3 && errorMessage"
+            >
+              {{ errorMessage }}
+            </span>
             <div class="flex flex-wrap">
               <label
                 v-for="Avatar in Avatars"
                 :for="Avatar.name"
                 :key="Avatar.id"
+                class="flex justify-center items-center"
               >
                 <img
                   :src="Avatar.path"
@@ -61,7 +86,10 @@
                   :name="Avatar.name"
                   :for="Avatar.name"
                   :id="Avatar.name"
+                  :value="Avatar.path"
                   class="hidden"
+                  v-model="displayPicture"
+                  @input="checkDisplayPicture"
                 />
               </label>
             </div>
@@ -70,6 +98,12 @@
             <h1 class="text-dark-text sm:text-xs lg:text-sm 2xl:text-lg">
               Display Theme
             </h1>
+            <span
+              class="sm:text-xs text-red-400 ml-3"
+              v-if="errorMessageRef === 4 && errorMessage"
+            >
+              {{ errorMessage }}
+            </span>
             <div class="flex flex-wrap">
               <label
                 v-for="Theme in Themes"
@@ -92,7 +126,10 @@
                   :name="Theme.name"
                   :for="Theme.name"
                   :id="Theme.name"
+                  :value="Theme.path"
                   class="hidden"
+                  v-model="displayTheme"
+                  @input="checkDisplayTheme"
                 />
               </label>
             </div>
@@ -111,6 +148,14 @@ import BaseButton from "../Base/BaseButton.vue";
 
 const selectedAvatar = ref(null);
 const selectedTheme = ref(null);
+
+const username = ref();
+const displayName = ref();
+const displayPicture = ref();
+const displayTheme = ref();
+
+const errorMessage = ref();
+const errorMessageRef = ref();
 
 const Avatars = ref([
   {
@@ -146,32 +191,32 @@ const Avatars = ref([
 ]);
 const Themes = ref([
   {
-    id: 1,
+    id: 11,
     name: "Theme1",
     path: "https://ik.imagekit.io/8boruzan4f9/Theme/Rectangle_18_QiOhI3_EYJ.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1661005964343",
   },
   {
-    id: 2,
+    id: 22,
     name: "Theme2",
     path: "https://ik.imagekit.io/8boruzan4f9/Theme/Rectangle_41_dY7cVK8mHw.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1661005964344",
   },
   {
-    id: 3,
+    id: 33,
     name: "Theme3",
     path: "https://ik.imagekit.io/8boruzan4f9/Theme/Rectangle_36_wGfeihWW7.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1661005964381",
   },
   {
-    id: 4,
+    id: 44,
     name: "Theme4",
     path: "https://ik.imagekit.io/8boruzan4f9/Theme/Rectangle_2_uwNtJ36sn.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1661005964382",
   },
   {
-    id: 5,
+    id: 55,
     name: "Theme5",
     path: "https://ik.imagekit.io/8boruzan4f9/Theme/Rectangle_28_EMCFT0uE4.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1661005964322",
   },
   {
-    id: 6,
+    id: 66,
     name: "Theme6",
     path: "https://ik.imagekit.io/8boruzan4f9/Theme/Rectangle_21_svixS6erGq.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1661005964317",
   },
@@ -179,8 +224,86 @@ const Themes = ref([
 
 const AvatarClicked = (e) => {
   selectedAvatar.value = e.target.dataset.id;
+  displayPicture.value = e.target.parentElement.lastChild.value
+  checkDisplayPicture()
 };
 const themeClicked = (e) => {
   selectedTheme.value = e.target.dataset.id;
+  displayTheme.value = e.target.parentElement.lastChild.value
+  checkDisplayTheme()
+};
+
+const checkUsername = () => {
+  // send request to firestore to check if there are any matching usernames
+  return true;
+};
+
+const checkDisplayName = () => {
+  if (displayName.value && displayName.value.length > 2) {
+    errorMessage.value = null;
+    errorMessageRef.value = null;
+    return true;
+  } else {
+    errorMessage.value = "Display Name should be atleast 3 letters";
+    errorMessageRef.value = 2;
+    return false;
+  }
+};
+
+const checkDisplayPicture = () => {
+  if (displayPicture.value) {
+    errorMessage.value = null;
+    errorMessageRef.value = null;
+    return true;
+  } else {
+    errorMessage.value = "Please Select a Display Picture";
+    errorMessageRef.value = 3;
+    return false;
+  }
+};
+const checkDisplayTheme = () => {
+  if (displayTheme.value) {
+    errorMessage.value = null;
+    errorMessageRef.value = null;
+    return true;
+  } else {
+    errorMessage.value = "Please Select a Display Theme";
+    errorMessageRef.value = 4;
+    return false;
+  }
+};
+
+const validation = () => {
+  if (checkUsername()) {
+    if (checkDisplayName()) {
+      if (checkDisplayPicture()) {
+        if (checkDisplayTheme()) {
+          return true;
+        } else {
+          console.log(errorMessage.value);
+          return false;
+        }
+      } else {
+        console.log(errorMessage.value);
+        return false;
+      }
+    } else {
+      console.log(errorMessage.value);
+      return false;
+    }
+  } else {
+    console.log(errorMessage.value);
+    return false;
+  }
+};
+
+const formSubmit = () => {
+  if (validation()) {
+    console.log("everything is good");
+    console.log(username.value);
+    console.log(displayName.value);
+    console.log(displayPicture.value);
+    console.log(displayTheme.value);
+  }
 };
 </script>
